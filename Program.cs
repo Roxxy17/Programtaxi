d7519ffe490ca7b7ba4b3f1f5b137118432ac31f
Roxxy17 <kalilaatha8@gmail.com>
2023-03-29 19:49:55 +0700

    Program taxi Class And Object

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi = new taxi();
+
+            taxi.DriverName = "Kalila Atha Achmad";
+            taxi.OnDuty = false;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+           
+        }
+      
+
+    }
+}
---
+++ TaxiClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTaxi
+{
+    public class taxi
+    {
+        //Properties
+        public string DriverName{ get; set; }
+        public int NumPassenger { get; set; }
+        public bool OnDuty { get; set; }
+            
+        //Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("Number of Pasangger {0}", NumPassenger);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty : No");
+            }
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang\n", DriverName);
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} sedang Mengantar penumpang\n", DriverName);
+        }
+      
+   }
+}
